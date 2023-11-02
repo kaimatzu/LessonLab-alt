03845d714ead50c183aa2a9ef3a654984502ba0b
HoneyHanny <hansduran25@gmail.com>
2023-11-02 20:53:51 +0800

    Fix plus button

--- src/Apps/LessonLab/Entry/View/Upload/Plus.rs
+++ src/Apps/LessonLab/Entry/View/Upload/Plus.rs
@@ -4,7 +4,9 @@ use dioxus::prelude::*;
 
 pub fn Plus(cx: Scope/*, onClick: () */) -> Element {
 	cx.render(rsx! { style { include_str!("../../../../../../assets/style.css") }
-		"+"
+		div { class: "plus",
+			"+"
+		}
 		// div { class: "plus",
 		// 	onclick: move |_| println!("Clicked"), "+"
 		// }
