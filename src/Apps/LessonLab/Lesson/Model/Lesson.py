feeb18ed75af3d460b5e47da6bfe9ba22927612a
Yuurei <markyuri.geagonia@cit.edu>
2023-11-03 13:42:08 +0800

    Change aspose to pymupdf

---
+++ src/Apps/LessonLab/Lesson/Model/Lesson.py
@@ -0,0 +1,11 @@
+class Lesson:
+    Title:str
+    KeyTopic:str
+    TimeFrame:str
+    LearningOutcome:str
+
+    def __init__(self, title:str, keyTopic:str, timeFrame:str, outcome:str) -> None:
+        Title = title
+        KeyTopic = keyTopic
+        TimeFrame = timeFrame
+        LearningOutcome = outcome
