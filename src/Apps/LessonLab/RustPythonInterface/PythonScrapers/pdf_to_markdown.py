107c95e9b16265e8380bd89c09c4638a09f0b509
Yuurei <markyuri.geagonia@cit.edu>
2023-11-03 13:42:08 +0800

    Change aspose to pymupdf

--- src/Apps/LessonLab/RustPythonInterface/PythonScrapers/pdf_to_markdown.py
+++ src/Apps/LessonLab/RustPythonInterface/PythonScrapers/pdf_to_markdown.py
@@ -1,52 +1,20 @@
-import pdfplumber
-import aspose.words as aw
-import openai
-import re
-from dotenv import load_dotenv
-import os
+import sys
+import fitz
 
-with pdfplumber.open('Design patterns Midterm Notes.pdf') as pdf:
-    # iterate over each page
-    for page in pdf.pages:
-        # extract text
-        text = page.extract_text()
-        # print(text)
+def pdf_to_markdown():
 
-fileName = "Design patterns Midterm Notes.pdf"
+  fileName = sys.argv[1]
 
-output = aw.Document()
-output.remove_all_children()
+  fileName = "Test.pdf"
 
-input = aw.Document(fileName)
-output.append_document(input, aw.ImportFormatMode.KEEP_SOURCE_FORMATTING)
+  pdf_document = fitz.open(fileName)
 
-output.save("Output.md")
+  markdown_text = ""
 
-with open("Output.md", "r", encoding="utf-8") as file:
-    markdown_text = file.read()
+  for page_number in range(pdf_document.page_count):
+      page = pdf_document[page_number]
+      page_text = page.get_text()
+      markdown_text += page_text
 
-    markdown_text = re.sub(r'!\[.*\]\(.*\)', '', markdown_text)
 
-    watermark_text_start = r"\*\*Evaluation Only\. Created with Aspose\.Words\. Copyright 2003-2023 Aspose Pty Ltd\.\*\*"
-    markdown_text = re.sub(watermark_text_start, '', markdown_text)
-
-    watermark_text_end = r"\*\*Created with an evaluation copy of Aspose\.Words\. To discover the full versions of our APIs please visit: https://products\.aspose\.com/words/\*\*"
-    markdown_text = re.sub(watermark_text_end, '', markdown_text)
-    #print(markdown_text)
-
-load_dotenv()
-
-openai.api_key = os.getenv("API_KEY")
-
-prompt = "Can you make a markdown format lesson based on this source: " + markdown_text
-
-completion = openai.ChatCompletion.create(
-  model="gpt-3.5-turbo",
-  messages=[
-    {"role": "system", "content": "You are a college teacher."},
-    {"role": "user", "content": prompt}
-  ]
-)
-
-#completion['choices'][0]['message']['content'] - the reply of gpt
-print(completion['choices'][0]['message']['content'])
+pdf_to_markdown()
